06898050ff08608462d55364c7cec092e8dc2fd3
shayuan <shayuan@silvrr.com>
2019-01-24 17:57:16 +0800

    m: update

--- ModuleB/Classes/ModuleB.swift
+++ ModuleB/Classes/ModuleB.swift
@@ -1,12 +1,13 @@
 import SwiftyMediator
 import MediatorTypes
 
-extension ModuleBMediatorType {
+extension ModuleBMediatorType: MediatorSourceType {
     public var viewController: UIViewController? {
         switch self {
         case .personal(let color):
             let vc = UIViewController()
             vc.view.backgroundColor = color
+            vc.title = "Personal"
             return vc
             
         case let .showAlert(title, message):
